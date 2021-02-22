d49f768391fde9e2eb2610437a94720ce97ed3e9
marina.oleinik <marina.oleinik@HP-CZC6297BB4>
2021-02-22 08:46:03 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace DBApp_TARgv20
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toodebox = new System.Windows.Forms.TextBox();
+            this.kogusbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hindbox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.filebox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.addbtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label1.Location = new System.Drawing.Point(289, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Andmebaas";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(258, 81);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(530, 243);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label2.Location = new System.Drawing.Point(13, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 39);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Toode";
+            // 
+            // toodebox
+            // 
+            this.toodebox.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.toodebox.Location = new System.Drawing.Point(136, 81);
+            this.toodebox.Name = "toodebox";
+            this.toodebox.Size = new System.Drawing.Size(116, 47);
+            this.toodebox.TabIndex = 3;
+            // 
+            // kogusbox
+            // 
+            this.kogusbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.kogusbox.Location = new System.Drawing.Point(136, 134);
+            this.kogusbox.Name = "kogusbox";
+            this.kogusbox.Size = new System.Drawing.Size(116, 47);
+            this.kogusbox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label3.Location = new System.Drawing.Point(12, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 39);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Kogus";
+            // 
+            // hindbox
+            // 
+            this.hindbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.hindbox.Location = new System.Drawing.Point(136, 186);
+            this.hindbox.Name = "hindbox";
+            this.hindbox.Size = new System.Drawing.Size(116, 47);
+            this.hindbox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label4.Location = new System.Drawing.Point(39, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 39);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Hind";
+            // 
+            // filebox
+            // 
+            this.filebox.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.filebox.Location = new System.Drawing.Point(136, 239);
+            this.filebox.Name = "filebox";
+            this.filebox.Size = new System.Drawing.Size(116, 47);
+            this.filebox.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label5.Location = new System.Drawing.Point(54, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 39);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "File";
+            // 
+            // addbtn
+            // 
+            this.addbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.addbtn.Location = new System.Drawing.Point(258, 330);
+            this.addbtn.Name = "addbtn";
+            this.addbtn.Size = new System.Drawing.Size(141, 49);
+            this.addbtn.TabIndex = 10;
+            this.addbtn.Text = "Lisa";
+            this.addbtn.UseVisualStyleBackColor = true;
+            this.addbtn.Click += new System.EventHandler(this.addbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.addbtn);
+            this.Controls.Add(this.filebox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.hindbox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.kogusbox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.toodebox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "DataBase";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox toodebox;
+        private System.Windows.Forms.TextBox kogusbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox hindbox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox filebox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button addbtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBApp_TARgv20
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=|DataDirectory|\AppData\Database.mdf;Integrated Security = True");
+        SqlDataAdapter adapter;
+        SqlCommand command;
+        public Form1()
+        {
+            InitializeComponent();
+            Data();
+        }
+        private void Data()
+        {
+            con.Open();
+            DataTable table = new DataTable();
+            adapter = new SqlDataAdapter("SELECT * FROM Tooded", con);
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+            con.Close();
+        }
+        int Id = 0;
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            Id = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            toodebox.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            kogusbox.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            hindbox.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            
+            if (dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString()=="")
+            {
+                filebox.Text = "none";
+            }
+            else
+            {
+                filebox.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+            }
+
+        }
+
+        private void addbtn_Click(object sender, EventArgs e)
+        {
+
+            con.Open();
+            command = new SqlCommand("INSERT INTO Tooded(Nimetus, Kogus, Hind, Pilt) VALUES(@toode,@kogus,@hind,@pilt)",con);
+            command.Parameters.AddWithValue("@toode", toodebox.Text);
+            command.Parameters.AddWithValue("@kogus", kogusbox.Text);
+            command.Parameters.AddWithValue("@hind", hindbox.Text);
+            //string file = "file.jpg";
+            command.Parameters.AddWithValue("@pilt", filebox.Text);
+            command.ExecuteNonQuery();
+            con.Close();
+            Data();
+        }
+    }
+}
